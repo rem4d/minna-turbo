23e920ff0555b25ceed62e92173dfb7a08b1a709
Jane <bleazint@gmail.com>
2025-03-07 11:52:31 +0300

    Fix typo

--- apps/client-front/src/routes.tsx
+++ apps/client-front/src/routes.tsx
@@ -27,7 +27,7 @@ const RouteErrorBoundary = () => {
     return (
       <div className="bg-athens-gray relative h-screen w-screen">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-black">
-          Connection error has occurered.
+          Connection error has occurred.
         </div>
       </div>
     );
