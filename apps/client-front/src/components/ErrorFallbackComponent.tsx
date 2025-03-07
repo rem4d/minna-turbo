9b0cb237f843672ac55c845dd224e8ccf316c077
Jane <bleazint@gmail.com>
2025-03-07 11:22:35 +0300

    Update error screens

--- apps/client-front/src/components/ErrorFallbackComponent.tsx
+++ apps/client-front/src/components/ErrorFallbackComponent.tsx
@@ -1,7 +1,9 @@
 export default function ErrorFallbackComponent() {
   return (
-    <div className="bg-athens-gray h-full w-full text-black">
-      <div className="text-center">Unexpected error has occurred</div>
+    <div className="bg-athens-gray relative h-screen w-screen">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-black">
+        Unexpected error has occurered.
+      </div>
     </div>
   );
 }
--- apps/client-front/src/routes.tsx
+++ apps/client-front/src/routes.tsx
@@ -25,8 +25,8 @@ const RouteErrorBoundary = () => {
 
   if (error instanceof TRPCClientError) {
     return (
-      <div className="bg-athens-gray w-full">
-        <div className="text-center text-black">
+      <div className="bg-athens-gray relative h-screen w-screen">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-black">
           Connection error has occurered.
         </div>
       </div>
