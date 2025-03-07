1300a48beb12600ab9c09d5499db91f56ee05d6e
Jane <bleazint@gmail.com>
2025-03-07 11:31:43 +0300

    Fix linter errors

--- apps/client-front/src/pages/IndexPage/IndexPage.tsx
+++ apps/client-front/src/pages/IndexPage/IndexPage.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 
 export const IndexPage: FC = () => {
+  console.log("init");
   return (
     <Page footer>
       <div className="flex flex-col items-center justify-center space-y-6 p-[33px]">
