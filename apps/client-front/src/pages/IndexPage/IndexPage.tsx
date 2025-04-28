b474fb973bc4940b8c250cb453afeba58f6f7f1e
Jane <bleazint@gmail.com>
2025-04-28 13:53:08 +0300

    Index page design upd

--- apps/client-front/src/pages/IndexPage/IndexPage.tsx
+++ apps/client-front/src/pages/IndexPage/IndexPage.tsx
@@ -1,4 +1,6 @@
-import type { FC } from "react";
+import type { FC, ReactElement } from "react";
+import BubbleIcon from "@/assets/images/bubble2.svg?react";
+import DeckIcon from "@/assets/images/deck.svg?react";
 import { Page } from "@/components/Page";
 import { useNavigate } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
@@ -13,12 +15,23 @@ export const IndexPage: FC = () => {
             desc="Читайте и слушайте реальные фразы на японском, состоящие только из изученных вами кандзи"
             to="/sentences"
             color="yellow"
+            icon={
+              <div className="absolute -right-2 -bottom-2 z-0 w-[40%] opacity-40">
+                {/* <BubbleIcon className="size-full" /> */}
+                <BubbleIcon className="size-full" />
+              </div>
+            }
           />
           <Card
             title="Карточки"
-            desc="Создавайте колоды карт из кандзи"
+            desc="Повышайте свой уровень, изучая новые кандзи с помощью карточек"
             to="/flashcards"
             color="cyan"
+            icon={
+              <div className="absolute right-4 bottom-0 z-0 w-[70px] opacity-40">
+                <DeckIcon className="size-full" />
+              </div>
+            }
           />
         </div>
       </div>
@@ -31,9 +44,10 @@ interface CardProps {
   desc: string;
   to: string;
   color: "yellow" | "cyan";
+  icon: ReactElement;
 }
 
-const Card: FC<CardProps> = ({ title, desc, to, color }) => {
+const Card: FC<CardProps> = ({ title, desc, to, color, icon }) => {
   const navigate = useNavigate();
   const handleButtonClick = () => {
     void navigate(to);
@@ -41,19 +55,20 @@ const Card: FC<CardProps> = ({ title, desc, to, color }) => {
   return (
     <div
       className={twMerge(
-        "border-heatheredGray/40 relative flex h-fit w-full flex-col space-y-4 rounded-xl border p-4 pt-(--page-offset-top-full)",
+        "border-heatheredGray/40 relative flex h-[200px] w-full flex-col justify-between space-y-4 overflow-hidden rounded-[20px] border p-6 shadow-md",
         color === "cyan" ? "bg-polar" : "bg-half-dutch-white",
       )}
       onClick={handleButtonClick}
     >
-      <div className="flex flex-col space-y-2">
+      {icon}
+      <div className="relative z-10 flex flex-col space-y-2">
         <div className="text-2xl font-semibold text-black">{title}</div>
         <div className="text-boulder text-sm">{desc}</div>
       </div>
       <button
         className={twMerge(
-          "relative bottom-2 mt-2 w-[130px] self-end rounded-full py-2 text-base font-semibold text-black",
-          color === "yellow" && "bg-supernova text-black",
+          "relative w-[50%] self-start rounded-[14px] py-2 text-base font-semibold text-black",
+          color === "yellow" && "bg-[#ffd664] text-black",
           color === "cyan" && "bg-scooter text-white",
         )}
       >
