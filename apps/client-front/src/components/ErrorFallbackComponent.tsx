513c95b94862935c7b7d02f8e87a0fd5d2a4bc9d
Jane <bleazint@gmail.com>
2025-04-29 12:33:26 +0300

    Minor style fixes

--- apps/client-front/src/components/ErrorFallbackComponent.tsx
+++ apps/client-front/src/components/ErrorFallbackComponent.tsx
@@ -2,7 +2,7 @@ export default function ErrorFallbackComponent() {
   return (
     <div className="bg-athens-gray relative h-screen w-screen">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-black">
-        Unexpected error has occurered.
+        Unexpected error has occurred.
       </div>
     </div>
   );
