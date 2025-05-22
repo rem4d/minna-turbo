a0f3219625b4e554d95646b99701065d79c909bc
Jane <bleazint@gmail.com>
2025-05-22 09:25:33 +0300

    Test deploy

--- apps/client-backend/src/index.ts
+++ apps/client-backend/src/index.ts
@@ -5,6 +5,7 @@ const server = app.listen(env.PORT, () => {
   const { HOST, PORT } = env;
   logger.info(`Server running on http://${HOST}:${PORT}`);
 });
+console.log(1);
 
 const onCloseSignal = () => {
   logger.info("sigint received, shutting down");
