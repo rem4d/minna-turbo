6c685544c7e3e09df638128cc76eeed1c8ff4881
Jane <bleazint@gmail.com>
2025-08-15 11:20:04 +0300

    Minor text upd

--- apps/client-front/src/components/ErrorFallbackComponent.tsx
+++ apps/client-front/src/components/ErrorFallbackComponent.tsx
@@ -2,7 +2,8 @@ export default function ErrorFallbackComponent() {
   return (
     <div className="bg-athens-gray relative h-screen w-screen">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-black">
-        Unexpected error has occurred.
+        Unexpected error has occurred. <br />
+        Please refresh page.
       </div>
     </div>
   );
