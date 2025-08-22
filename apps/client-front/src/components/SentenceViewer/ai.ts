b677e423829b3eddddbec56dbc4a896c82808a1d
Jane <bleazint@gmail.com>
2025-08-22 12:27:19 +0300

    Minor updates

--- apps/client-front/src/components/SentenceViewer/ai.ts
+++ apps/client-front/src/components/SentenceViewer/ai.ts
@@ -4,10 +4,12 @@ const mClient = new Mistral({
   apiKey: import.meta.env.VITE_MISTRAL_API_KEY,
 });
 
-const model = "mistral-large-latest";
+// const model = "mistral-large-latest";
+const model = "magistral-medium-2506";
 
 export const runStream = async (text: string) => {
-  const prompt = `'${text}' разбери грамматику на русском. Если есть чтения, отобрази в виде хираганы. Не отображай чтения на русском.`;
+  // const prompt = `'${text}' разбери грамматику на русском. Если есть чтения, отобрази в виде хираганы. Не отображай чтения на русском.`;
+  const prompt = `${text} разбери грамматику на русском. Если есть чтения, отобрази в виде хираганы. Не отображай чтения на русском.`;
   const response = mClient.beta.conversations.startStream({
     model,
     inputs: prompt,
