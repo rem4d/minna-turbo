2dbe7a6acf867913bf0c94e0c15fef7788d24702
Rem <bleazint@gmail.com>
2026-02-14 13:06:22 +0300

    Chore

--- packages/utils/src/shuffle.ts
+++ packages/utils/src/shuffle.ts
@@ -1,12 +1,10 @@
-export function shuffle<T>(array: T[]) {
+export function shuffle<T extends unknown[]>(array: T) {
   const tmp = [...array];
 
   for (let i = tmp.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     const c = tmp[i];
-    // @ts-expect-error noext
     tmp[i] = tmp[j];
-    // @ts-expect-error noext
     tmp[j] = c;
   }
   return tmp;
